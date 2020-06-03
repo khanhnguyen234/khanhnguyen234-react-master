2e30914dd763a1ef9c796f8679a02e1503970671
Khanh <khanhnh@sendo.vn>
2020-06-04 01:52:31 +0700

    feat: add khanhnguyen common

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,87 @@
 
 import * as React from 'react';
 import * as styles from './styles.scss';
+import { scrollToOffset } from '@khanhnguyen/react-common/src/utils/window'
 // import styles from './styles.scss';
 
 import PageInterface from '../PageInterface';
 
-class App extends React.Component<PageInterface, {}> {
-  render() {
-    return (
-      <div className={styles.body}>
-        <h1>React + Typescript + Webpack</h1>
-        <p>The color of this page is: {this.props.color}</p>
-      </div>
-    );
+const App = (props: PageInterface) => {
+  const scrollTop = () => {
+    scrollToOffset({top: 0})
   }
+  return (
+    <div className={styles.body}>
+      <h2 className={styles.notification}>This is top</h2>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <h1>React + Typescript + Webpack</h1>
+      <p>The color of this page is: {props.color}</p>
+
+      <button onClick={scrollTop}>
+        scrollToOffset
+      </button>
+    </div>
+  );
 }
 
 export default App;
