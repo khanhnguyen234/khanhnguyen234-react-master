16f5df4d33a42ca3699cbaab30f38b15eb731adf
Huynh Khanh Nguyen <fealess95@gmail.com>
2020-09-13 02:14:48 +0700

    fix: import scss, tmp disable service worker

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as styles from './styles.scss';
+import { scrollToOffset } from '@khanhnguyen234/react-common'
+import { ButtonExt, Title } from '@khanhnguyen234/react-components'
+
+const App = (props) => {
+  const scrollTop = () => {
+    scrollToOffset({top: 9000})
+  }
+
+  return (
+    <div className={styles.body}>
+      <h1>React + Typescript + Webpack + Workbox</h1>
+
+      <ButtonExt onClick={scrollTop} className={styles.btn}>
+        Default
+      </ButtonExt>
+      
+      <Title>
+        Title default (red color)
+      </Title>
+
+      <Title className={styles.title}>
+        Title has been override (green color)
+      </Title>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import App from './components/App';
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
+import App from './App';
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//       console.log('SW registered: ', registration);
+//     }).catch(registrationError => {
+//       console.log('SW registration failed: ', registrationError);
+//     });
+//   });
+// }
 
 ReactDOM.render(<App color="Blue" />, document.getElementById("root"));
